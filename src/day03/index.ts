042b740a4f078079112ef104a479b5f479c9d897
Luis Araujo <luis.araujo@nentgroup.com>
2022-07-28 13:45:31 +0200

    Day 03

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -1,37 +1,114 @@
 import fs from 'fs';
 import path from 'path';
 
-try {
-    console.log(path.join(__dirname + '/../../src/day03/input1.txt'));
-    const data = fs.readFileSync(path.join(__dirname + '/../../src/day03/input1.txt'), 'utf8');
+const array = readFile(path.join(__dirname + '/../../src/day03/input1.txt'));
+//console.log(array);
 
-    const array = [];
-    data.split(/\r?\n/).forEach(line => {
-        array.push(line);
-    });
-    console.log(array);
-
-    let gamma = '';
-    let epsilon = '';
-    let oneCounter = 0;
-    let zeroCounter = 0;
-    for (let i = 0; i < 12; i++) {
-        for (let j = 0; j < array.length; j++) {
-            const char = array[j].charAt(i);
-            if (char === '0') zeroCounter++
-            else oneCounter++;
-            if(i===4){console.log(char)}
-        }
-        
-        if (oneCounter > zeroCounter) { gamma += '1'; epsilon += '0' }
-        else { gamma += '0'; epsilon += '1' }
-        oneCounter = 0;
-        zeroCounter= 0
+// Part 1
+let gamma: string = '';
+let epsilon: string = '';
+let oneCounter: number = 0;
+let zeroCounter: number = 0;
+for (let i: number = 0; i < 12; i++) {
+    for (let j = 0; j < array.length; j++) {
+        const char: string = array[j].charAt(i);
+        if (char === '0') zeroCounter++
+        else oneCounter++;
     }
-    console.log('gamma', gamma); // 1174 in decimal
-    console.log('epsilon', epsilon); // 2921 in decimal
+    if (oneCounter > zeroCounter) {
+        gamma += '1'; epsilon += '0'
+    }
+    else { gamma += '0'; epsilon += '1' }
+    oneCounter = 0;
+    zeroCounter = 0
+}
+console.log('gamma', gamma, 'decimal', parseInt(gamma, 2)); // 1174 in decimal
+console.log('epsilon', epsilon, 'decimal', parseInt(epsilon, 2)); // 2921 in decimal
 
 
-} catch (err) {
-    console.error(err);
+// Part 2
+
+const result1 = loopArray(array, 0);
+const result2 = loopArray2(array, 0)
+console.log('result', result1, parseInt(result1, 2));
+
+console.log('result2', result2, parseInt(result2, 2));
+console.log('answer', parseInt(result2, 2) * parseInt(result1, 2));
+
+
+function readFile(filePath: string) {
+    try {
+        console.log(filePath);
+        const data = fs.readFileSync(filePath, 'utf8');
+
+        const array = [];
+        data.split(/\r?\n/).forEach(line => {
+            array.push(line);
+        });
+        return array;
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+}
+
+function loopArray(array: string[], i: number) {
+    const zeroArray: string[] = [];
+    const oneArray: string[] = [];
+    for (let j = 0; j < array.length; j++) {
+        const char = array[j].charAt(i);
+        if (char === '0')
+            zeroArray.push(array[j]);
+        else
+            oneArray.push(array[j]);
+    }
+    if (oneArray.length === zeroArray.length) {
+        if (oneArray.length > 1) {
+            return loopArray(oneArray, ++i);
+        }
+
+        if (oneArray[0].charAt(i) === '1') {
+            return oneArray[0];
+        } else {
+            return zeroArray[0];
+        }
+
+    } else if (oneArray.length > zeroArray.length) {
+        return loopArray(oneArray, ++i);
+    } else {
+        return loopArray(zeroArray, ++i);
+    }
+}
+
+
+function loopArray2(array: string[], i: number) {
+    const zeroArray: string[] = [];
+    const oneArray: string[] = [];
+    for (let j = 0; j < array.length; j++) {
+        const char = array[j].charAt(i);
+        if (char === '0')
+            zeroArray.push(array[j]);
+        else
+            oneArray.push(array[j]);
+    }
+    if (oneArray.length === zeroArray.length) {
+        if (zeroArray.length > 1) {
+            return loopArray2(zeroArray, ++i);
+        }
+        if (oneArray[0].charAt(i) === '0') {
+            return oneArray[0];
+        } else {
+            return zeroArray[0];
+        }
+
+    }
+    else if (oneArray.length < zeroArray.length && oneArray.length > 0) {
+        return loopArray2(oneArray, ++i);
+    } if (oneArray.length > zeroArray.length && zeroArray.length > 0) {
+        return loopArray2(zeroArray, ++i);
+    } else if (zeroArray.length === 0 && oneArray.length === 1) {
+        return oneArray[0]
+    } else {
+        return zeroArray[0];
+    }
 }
