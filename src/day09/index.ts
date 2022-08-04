1a057b0215678f468c673408c0fd922ab5106def
Luis Araujo <luis.araujo@nentgroup.com>
2022-08-04 16:24:59 +0200

    day 09

--- src/day09/index.ts
+++ src/day09/index.ts
@@ -65,8 +65,8 @@ function processPart1(array: number[][]) {
     return counter;
 }
 
-
 function processPart2(array: number[][]) {
+    const result = []
 
     let counter = 0;
     const height: number = array.length
@@ -74,25 +74,62 @@ function processPart2(array: number[][]) {
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
             const value = array[y][x];
+            const { up, down, right, left } = getBasis(y, x, array);
 
-            const { up, down, right, left } = getAdjacentLocationValues(y, x, array);
-            if (value < up && value < down && value < left && value < right)
+            if (value < up && value < down && value < left && value < right) {
+                const basinSet: Set<string> = new Set();
+                const basisResultSet = findLargestBasis(y, x, value, array, basinSet);
+                result.push(basisResultSet.size + 1);
                 counter = counter + value + 1;
+            }
 
 
         }
 
     }
-    console.log('Part 2 Result: ', { counter });
+    result.sort((a, b) => b - a)
+    console.log({ result }, result.length);
+    console.log('counter', counter);
 
-    return counter;
+    console.log('Part 2 Result: ', result[0] * result[1] * result[2]);
+
+    return result;
 }
 
 
 
 processPart2(array);
 
-function getAdjacentLocationValues(y: number, x: number, array: number[][]) {
+
+
+function findLargestBasis(y: number, x: number, value: number, array: number[][], basisResultSet: Set<string>): Set<string> {
+    let up = width;
+    let down = width;
+    let right = width;
+    let left = width;
+    ({ up, down, right, left } = getBasis(y, x, array));
+
+    const nextValue = value + 1;
+    if (up === nextValue && nextValue != 9) {
+        basisResultSet.add((y - 1) + '-' + x);
+        findLargestBasis(y - 1, x, nextValue, array, basisResultSet);
+    }
+    if (down === nextValue && nextValue != 9) {
+        basisResultSet.add((y + 1) + '-' + x);
+        findLargestBasis(y + 1, x, nextValue, array, basisResultSet);
+    }
+    if (right === nextValue && nextValue != 9) {
+        basisResultSet.add(y + '-' + (x + 1));
+        findLargestBasis(y, x + 1, nextValue, array, basisResultSet);
+    }
+    if (left === nextValue && nextValue != 9) {
+        basisResultSet.add(y + '-' + (x - 1));
+        findLargestBasis(y, x - 1, nextValue, array, basisResultSet);
+    }
+    return basisResultSet;
+}
+
+function getBasis(y: number, x: number, array: number[][]) {
     const width: number = array[0].length;
     let up = width;
     let down = width;
@@ -107,8 +144,8 @@ function getAdjacentLocationValues(y: number, x: number, array: number[][]) {
     if (x + 1 < width)
         right = array[y][x + 1];
 
-    if (x > 0) {
+    if (x > 0)
         left = array[y][x - 1];
-    }
+
     return { up, down, right, left };
 }
