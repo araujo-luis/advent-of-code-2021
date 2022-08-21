132fad9ea1c631e6e532d4f98e229880c69f9efb
Luis Araujo <luis.araujo@nentgroup.com>
2022-08-21 16:07:02 +0200

    refactor day14

--- src/day14/index.ts
+++ src/day14/index.ts
@@ -29,82 +29,58 @@ function readFile(filePath: string) {
 
 const { map, string } = readFile(path.join(__dirname + '/../../src/day14/input1.txt'));
 //console.log({ map, string })
-const counterMap = new Map<string, number>();
-
-
-const counterPairs = new Map<string, number>();
+const counterCharatetsMapPart1 = new Map<string, number>();
 string.split('').forEach((x) => {
-    counterMap.set(x, (counterMap.get(x) || 0) + 1)
+    counterCharatetsMapPart1.set(x, (counterCharatetsMapPart1.get(x) || 0) + 1)
 });
+const counterCharatetsMapPart2 = new Map(counterCharatetsMapPart1);
+
+
+const counterPairsPart1 = new Map<string, number>();
+
 for (let i = 0; i < string.length; i++) {
     if (i + 2 > string.length)
         break
     const pair = string.substring(i, i + 2);
-    counterPairs.set(pair, (counterPairs.get(pair) || 0) + 1)
+    counterPairsPart1.set(pair, (counterPairsPart1.get(pair) || 0) + 1)
 }
 
-
-function processPart1(string: string, runs: number, counter: number = 0) {
-    counter++;
-    const stringAsArray = string.split('');
-    //console.log({ stringAsArray });
-    let finalString = '';
-
-    for (let j = 0; j < stringAsArray.length; j++) {
-        if (j + 1 >= stringAsArray.length)
-            break;
-        const code = stringAsArray[j] + stringAsArray[j + 1];
-        const value = map.get(code);
-        if (value) {
-            finalString += value + stringAsArray[j + 1];
-            counterMap.set(value, (counterMap.get(value) || 0) + 1)
-        }
-        else
-            finalString += stringAsArray[j] + stringAsArray[j + 1];
-    }
-    finalString = stringAsArray[0] + finalString;
-    console.log({ finalString })
-    if (counter < runs)
-        processPart1(finalString, runs, counter)
-
-    return finalString;
-
-}
-
-
-function processPart2(stringPairs: Map<string, number>, runs: number, counter: number = 0) {
+const counterPairsPart2 = new Map(counterPairsPart1);
+function processBothParts(stringPairs: Map<string, number>, runs: number, counterCharacterMap: Map<string, number>, counter: number = 0) {
     counter++;
 
     const newMap = new Map();
-    //console.log({ newMap, stringPairs });
 
     for (let pair of stringPairs.keys()) {
         const value = map.get(pair);
         if (!value)
             continue
 
-        counterMap.set(value, ((counterMap.get(value) || 0) + 1)
-            + stringPairs.get(pair) - 1);
+        const ocurrences = stringPairs.get(pair);
+        counterCharacterMap.set(value, ((counterCharacterMap.get(value) || 0) + 1) + ocurrences - 1);
         const firstNewPair = pair[0] + value;
         const secondNewPair = value + pair[1];
 
-        newMap.set(firstNewPair, ((newMap.get(firstNewPair) || 0) + 1) + stringPairs.get(pair) - 1);
-        newMap.set(secondNewPair, ((newMap.get(secondNewPair) || 0) + 1) + stringPairs.get(pair) - 1);
+        newMap.set(firstNewPair, ((newMap.get(firstNewPair) || 0) + 1) + ocurrences - 1);
+        newMap.set(secondNewPair, ((newMap.get(secondNewPair) || 0) + 1) + ocurrences - 1);
     }
 
     if (counter < runs) {
-        console.log({ counter });
-
-        processPart2(newMap, runs, counter)
+        processBothParts(newMap, runs, counterCharacterMap, counter)
     }
 
     return newMap;
 }
 
-console.log({ counterPairs })
-console.log('resutl', processPart2(counterPairs, 40));
+
+processBothParts(counterPairsPart1, 10, counterCharatetsMapPart1);
+processBothParts(counterPairsPart2, 40, counterCharatetsMapPart2);
 
 
-const max = Math.max(...counterMap.values());
-const min = Math.min(...counterMap.values())
-console.log(counterMap, { max, min, result: max - min });
+const maxPart1 = Math.max(...counterCharatetsMapPart1.values());
+const minPart1 = Math.min(...counterCharatetsMapPart1.values())
+
+const maxPart2 = Math.max(...counterCharatetsMapPart2.values());
+const minPart2 = Math.min(...counterCharatetsMapPart2.values())
+console.log('Result Part 1', { maxPart1, minPart1, result: maxPart1 - minPart1 });
+console.log('Result Part 2', { maxPart2, minPart2, result: maxPart2 - minPart2 });
